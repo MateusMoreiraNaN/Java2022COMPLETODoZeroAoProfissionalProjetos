d0bb18ec00ea21da6157bdd3a37b8c2765610ae5
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2022-09-20 17:36:23 -0300

    notação de ponto

---
+++ src/fundamentos/Aula05NotaçãoPonto.java
@@ -0,0 +1,34 @@
+package fundamentos;
+
+import java.util.Locale;
+
+public class Aula05NotaçãoPonto {
+    public static void main(String[] args) {
+
+        double a = 2.4;
+        String s = "Bom dia X";
+        s = s.replace("X", "Senhora");
+
+        s = s.toUpperCase();
+        s = s.concat("!!!.");
+
+        System.out.println(s);
+
+        String x = "leo".
+                toUpperCase
+                        ();
+
+        System.out.println(x);
+
+        String y = "Bom dia X".
+                replace("X", "Gui");
+        System.out.println(y);
+
+        String m = "MATEUS".toLowerCase();
+        System.out.println(m);
+
+        //int soma = 45;
+
+
+    }
+}
