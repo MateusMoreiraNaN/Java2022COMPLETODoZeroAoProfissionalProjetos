4c1c874702ea095441de075e5003140efefaf8f9
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2022-09-20 18:15:00 -0300

    imports

---
+++ src/fundamentos/Aula06Import.java
@@ -0,0 +1,22 @@
+package fundamentos;
+
+import javax.swing.*;
+import java.util.Date;
+
+
+public class Aula06Import {
+    public static void main(String[] args) {
+        java.lang.String b = "Boa tarde";
+        System.out.println(b);
+
+        String s = "bom dia";
+
+        System.out.println(s);
+
+        Date d = new Date();
+
+        System.out.println(d);
+
+        //JButton botao = new JButton();
+    }
+}
