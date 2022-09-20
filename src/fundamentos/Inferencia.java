180e250d8746dbf2359c12a093bd28b607e9f089
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2022-09-20 13:31:04 -0300

    inferencia em java

---
+++ src/fundamentos/Inferencia.java
@@ -0,0 +1,24 @@
+package fundamentos;
+
+public class Inferencia {
+    public static void main(String[] args) {
+            double a = 4.5;
+            System.out.println(a);
+
+            var b = 10;
+
+            var c = "Essa variavel não precisa colocar o tipo";
+            System.out.println(b);
+            //c = 4;
+            System.out.println(c);
+            var d = "Vou mudar essa varivel";
+            System.out.println(d);
+            d = "Mudei";
+            System.out.println(d);
+
+            String nome;
+            nome = "Mateus";
+            System.out.println(nome);
+    }
+
+}
