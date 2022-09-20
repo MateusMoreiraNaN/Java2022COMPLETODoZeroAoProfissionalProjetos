aa87874fc191ec301b5485c0294d724e59eeecaf
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2022-09-20 13:37:40 -0300

    inferencia em java2.0

--- src/fundamentos/Aula03Inferencia.java
+++ src/fundamentos/Aula03Inferencia.java
@@ -1,6 +1,6 @@
 package fundamentos;
 
-public class Inferencia {
+public class Aula03Inferencia {
     public static void main(String[] args) {
             double a = 4.5;
             System.out.println(a);
