3031485f97e02ddd29505182d3ce5839d3e762f3
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2022-09-21 15:19:40 -0300

    Objeto&Primitivo

---
+++ src/fundamentos/Aula09ObjetoVsPrimitivo.java
@@ -0,0 +1,15 @@
+package fundamentos;
+
+public class Aula09ObjetoVsPrimitivo {
+
+    public static void main(String[] args) {
+
+        String s = new String("Texto");
+        s.toUpperCase();
+
+        // Wrappers são a  versão objeto dos tipos
+        // primitivos!
+        int a = 123;
+        System.out.println(a);
+    }
+}
