089ec34f82e67ad431d12be58a562115417a19f2
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2022-09-21 14:33:04 -0300

    ConsolePraticarParte2

---
+++ src/fundamentos/Aula08ConsolePart2.java
@@ -0,0 +1,67 @@
+package fundamentos;
+
+import java.util.Scanner;
+
+public  class Aula08ConsolePart2 {
+    public static void main(String[] args) {
+        Scanner validador = new Scanner(System.in);
+
+        System.out.println("Email: ");
+        String email = validador.next();
+        String emailDB = "mateus2022";
+        System.out.println("Senha: ");
+        int senha = validador.nextInt();
+        int senhaDB = 1234;
+
+
+        if(emailDB == email){
+            System.out.println("Acesso Liberado");
+        }else if(senhaDB == senha){
+            System.out.println("Acesso Liberado");
+        }else if(emailDB != email && senhaDB != senha){
+            System.out.println("Acesso Negado");
+        }else if(email != emailDB && senha == senhaDB){
+            System.out.println("Email Incorreto");
+        }else if(email == emailDB && senha != senhaDB){
+            System.out.println("Senha Icorreta");
+        }
+
+    }
+}
+
+/*
+    class validador {
+        public static void main(String[] args) {
+
+
+            Scanner validador = new Scanner(System.in);
+
+
+
+            System.out.println("Email: ");
+            String email = validador.next();
+            String emailDB = "mateus2022";
+
+            System.out.println("Senha: ");
+            int senha = validador.nextInt();
+            int senhaDB = 1234;
+
+
+            if(emailDB == email){
+                System.out.println("Acesso Liberado");
+            }else if(senhaDB == senha){
+                System.out.println("Acesso Liberado");
+            }else if(emailDB != email && senhaDB != senha){
+                System.out.println("Acesso Negado");
+            }else if(email != emailDB && senha == senhaDB){
+                System.out.println("Email Incorreto");
+            }else if(email == emailDB && senha != senhaDB){
+                System.out.println("Senha Icorreta");
+            }
+
+
+
+        }
+    }
+*/
+
