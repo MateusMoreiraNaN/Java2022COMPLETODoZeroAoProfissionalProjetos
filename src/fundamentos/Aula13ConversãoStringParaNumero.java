ab4d21a6027a40f0cf55c22b8133d3e6438a5db0
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2022-09-22 15:39:01 -0300

    String para numero

---
+++ src/fundamentos/Aula13ConversãoStringParaNumero.java
@@ -0,0 +1,17 @@
+package fundamentos;
+
+import javax.swing.*;
+
+public class Aula13ConversãoStringParaNumero {
+    public static void main(String[] args) {
+        String valor1 = JOptionPane.showInputDialog("Digite o primeiro número: ");
+        String valor2 = JOptionPane.showInputDialog("Digite o segundo número: ");
+
+        System.out.println(valor1 + valor2);
+
+        int numero1 = Integer.parseInt(valor1);
+        int numero2 = Integer.parseInt(valor2);
+
+        System.out.println(numero1 + numero2);
+    }
+}
