a4af802665477f9c096b4adebfc32b6e066198f8
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2022-09-23 18:59:20 -0300

    OperadoresLogicosDesafio

---
+++ src/fundamentos/Aula15OperadoresLógicoDesafio.java
@@ -0,0 +1,39 @@
+package fundamentos;
+
+public class Aula15OperadoresLógicoDesafio {
+    public static void main(String[] args) {
+
+
+
+        boolean terca = true;
+        boolean quarta = false;
+
+        boolean S = true;
+        boolean N = false;
+
+
+        if(S == terca && S == quarta){
+            String Tv1 = "50 POLEGADAS";
+            System.out.println("VAMOS PRO SHOPPING NO FINAL DE SEMANA");
+            System.out.println("E COMPRAR UMA TV DE " + Tv1);
+            System.out.println("DEPOIS VAMOS IR TOMAR SORVETE");
+        }else if (terca == N && quarta == N) {
+            System.out.println("VAMOS FICAR EM CASA");
+            System.out.println("EE COM FOME KKKKKKKK");
+        }else if(terca == S || quarta == S){
+            String Tv2 = "32 POLEGADAS";
+            System.out.println("VAMOS PRO SHOPPING NO FINAL DE SEMANA");
+            System.out.println("E COMPRAR UMA TV DE " + Tv2);
+            System.out.println("DEPOIS VAMOS IR TOMAR SORVETE");
+        }else{
+            System.out.println("não entendi");
+        }
+
+
+
+
+
+
+
+    }
+}
