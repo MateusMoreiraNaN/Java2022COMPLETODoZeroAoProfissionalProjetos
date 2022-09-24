fbf183e6e70efe83766fdafa5690832ed8d38870
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2022-09-24 15:10:15 -0300

    DesafioIf

---
+++ src/EstruturasDeControle/Aula22DesafioIf.java
@@ -0,0 +1,18 @@
+package EstruturasDeControle;
+
+public class Aula22DesafioIf {
+    public static void main(String[] args) {
+        double nota = 1.3;
+
+        /*
+        if(nota >= 9.0); {
+            System.out.println("Quadro de Honra!");
+            System.out.println("Você é fera!!!");
+        }
+        */
+        if(nota >= 9.0) {
+            System.out.println("Quadro de Honra!");
+            System.out.println("Você é fera!!!");
+        }
+    }
+}
