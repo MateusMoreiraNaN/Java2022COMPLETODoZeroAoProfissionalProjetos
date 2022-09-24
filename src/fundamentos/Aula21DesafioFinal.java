008ff5780357c951a8f9d359e9748f922106c461
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2022-09-24 13:42:13 -0300

    DesafioFinal

---
+++ src/fundamentos/Aula21DesafioFinal.java
@@ -0,0 +1,40 @@
+package fundamentos;
+
+import java.util.Scanner;
+
+public class Aula21DesafioFinal {
+    public static void main(String[] args) {
+        Scanner calculadora = new Scanner(System.in);
+
+        System.out.print("Valor 1: ");
+        int n1 = calculadora.nextInt();
+
+        System.out.print("Valor 2: ");
+        int n2 = calculadora.nextInt();
+
+        System.out.print("Operação (+, -, *, /): ");
+        String op = calculadora.next();
+        String resultado = "";
+
+        Boolean mais = op.equals("+");
+        resultado = mais ? Integer.toString(n1 + n2) : resultado;
+
+        Boolean menos = op.equals("-");
+        resultado = menos ? Integer.toString(n1 - n2) : resultado;
+
+        Boolean vezes = op.equals("*");
+        resultado = vezes ? Integer.toString(n1 * n2) : resultado;
+
+        Boolean dividir = op.equals("/");
+        resultado = dividir ? Integer.toString(n1 / n2) : resultado;
+
+        System.out.println("Resultado da operação: " + resultado);
+
+
+
+
+
+
+        calculadora.close();
+    }
+}
