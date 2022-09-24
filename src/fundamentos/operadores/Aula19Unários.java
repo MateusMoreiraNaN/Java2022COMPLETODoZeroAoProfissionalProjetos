5b83da212013dbed52825955ccf9c65eb3ca9160
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2022-09-24 11:52:00 -0300

    Unários

---
+++ src/fundamentos/operadores/Aula19Unários.java
@@ -0,0 +1,21 @@
+package fundamentos.operadores;
+
+public class Aula19Unários {
+    public static void main(String[] args) {
+        double media = 3.6;
+        //String resuldadoParcial = media >= 5.0 ? "em recuperação" : "reprovado";
+        String resuldadoFinal = media >= 7.0 ?
+                "Aprovaddo." : "Em recuperação.";
+
+        System.out.println("O aluno está " + resuldadoFinal);
+
+        double nota = 8.3;
+        boolean bomComportamento = true;
+
+        boolean passouPorMeida = nota >= 7;
+        boolean temDesconto = bomComportamento && nota >= 7;
+        String resuldaddo = temDesconto ? "sim." : "não.";
+
+        System.out.println("Tem desconto? " + resuldaddo);
+    }
+}
