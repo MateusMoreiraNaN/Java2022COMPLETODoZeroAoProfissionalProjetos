c4d9ca055507297eab9b0027a9766e122e61fcb1
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2022-09-24 11:23:17 -0300

    Atribuição

---
+++ src/fundamentos/operadores/Aula17Atribuição.java
@@ -0,0 +1,24 @@
+package fundamentos.operadores;
+
+public class Aula17Atribuição {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = a;
+        int c = a + b;
+
+        c += b;//c = c + b;
+
+        c -= a; //c = c - a;
+
+        c *= b; // c = c * b;
+
+        c /= a; // c = c / a;
+
+        c %= 2; // c = c % 2 0 ou 1
+
+        c++; //c = c + 1;
+
+        System.out.println(c);
+
+    }
+}
