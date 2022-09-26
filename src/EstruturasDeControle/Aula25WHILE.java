78cb995a444232792d208d16714b06b64d076600
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2022-09-26 13:15:16 -0300

    WHILE

---
+++ src/EstruturasDeControle/Aula25WHILE.java
@@ -0,0 +1,25 @@
+package EstruturasDeControle;
+
+public class Aula25WHILE {
+    public static void main(String[] args) {
+        /*
+            System.out.println("Bom dia");
+            System.out.println("Bom dia");
+            System.out.println("Bom dia");
+            System.out.println("Bom dia");
+            System.out.println("Bom dia");
+            System.out.println("Bom dia");
+            System.out.println("Bom dia");
+            System.out.println("Bom dia");
+            System.out.println("Bom dia");
+            System.out.println("Bom dia");
+        */
+
+        int contador = 0;
+
+        while (contador <= 50){
+            System.out.printf("i = %d\n", contador);
+            contador+= 2;
+        }
+    }
+}
