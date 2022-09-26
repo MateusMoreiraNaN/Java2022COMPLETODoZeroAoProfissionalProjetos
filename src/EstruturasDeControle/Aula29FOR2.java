a1880d226d5262858216511af1f4eb1d39602bd3
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2022-09-26 14:55:02 -0300

    For2

---
+++ src/EstruturasDeControle/Aula29FOR2.java
@@ -0,0 +1,10 @@
+package EstruturasDeControle;
+
+public class Aula29FOR2 {
+    public static void main(String[] args) {
+
+        for(int con = 100; con >= 0; con -= 5){
+            System.out.println(con);
+        }
+    }
+}
