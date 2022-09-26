66db87a520617fc240470c56c30c8c9000d1f09c
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2022-09-26 17:02:15 -0300

    DesafioFor

---
+++ src/EstruturasDeControle/Aula31DesafioFor.java
@@ -0,0 +1,12 @@
+package EstruturasDeControle;
+
+public class Aula31DesafioFor {
+    public static void main(String[] args) {
+        String valor = "#";
+
+        for(String v = "#"; !v.equals("######"); v += "#"){
+            System.out.println(v);
+
+        }
+    }
+}
