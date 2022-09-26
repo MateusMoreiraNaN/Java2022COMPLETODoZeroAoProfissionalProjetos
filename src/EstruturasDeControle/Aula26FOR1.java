5c4f008e941bd7b3a137aa2f82fe24f836811df7
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2022-09-26 13:35:47 -0300

    FOR1

---
+++ src/EstruturasDeControle/Aula26FOR1.java
@@ -0,0 +1,27 @@
+package EstruturasDeControle;
+
+public class Aula26FOR1 {
+    public static void main(String[] args) {
+        for(int con = 0; con <= 20; con++){
+            System.out.printf("i = %d\n", con);
+
+        }
+
+        /*
+        int x = 2;
+        for(; x < 10;){
+            System.out.println("x= " + x);
+            x++;
+        }
+        */
+
+
+        // Laço infinito!!!
+        /*
+        for(;true;){
+            System.out.println("Fim!");
+        }
+        */
+
+    }
+}
