11c29a1d6467993398bdd6b9003a6de9f2892933
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2022-09-27 12:44:25 -0300

    data2

--- src/ClassesEMétodos/Aula34DataMain.java
+++ src/ClassesEMétodos/Aula34DataMain.java
@@ -6,9 +6,18 @@ public class Aula34DataMain {
         Aula34Data data = new Aula34Data();
 
         data.dia = 15;
-        data.mes = 9;
+        data.mes = 11;
         data.ano = 1914;
 
+        var data2 = new Aula34Data();
+
+        data2.dia = 27;
+        data2.mes = 9;
+        data2.ano = 2022;
+
         System.out.println(data.dia + "/" + data.mes + "/" + data.ano);
+        System.out.println(data2.dia + "/" + data2.mes + "/" + data2.ano);
+
+
     }
 }
