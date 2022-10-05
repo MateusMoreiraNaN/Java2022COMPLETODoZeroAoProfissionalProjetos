c87e87f4bc5ad9600aa020d355e9853e8d1631e8
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2022-10-05 13:40:08 -0300

    metosos

--- src/ClassesEMétodos/Aula33ClasseProduto.java
+++ src/ClassesEMétodos/Aula33ClasseProduto.java
@@ -5,4 +5,18 @@ public class Aula33ClasseProduto {
     String nome;
     double preco;
     double desconto;
+
+    /*
+    float precoComDesconto(){
+        return (float) (preco + (1 - desconto));
+    }
+    */
+
+    double precoComDesconto(){
+        return preco + (1 - desconto);
+    }
+
+    double precoComDesconto(double descontoDoGerente, double funcionarios){
+        return preco + (1 - descontoDoGerente);
+    }
 }
--- src/ClassesEMétodos/Aula33ProdutoTeste.java
+++ src/ClassesEMétodos/Aula33ProdutoTeste.java
@@ -21,11 +21,12 @@ public class Aula33ProdutoTeste {
         System.out.println(p1.nome);
         System.out.println(p2.nome);
 
-        double precoFinal1 = p1.preco * (1 - p1.desconto);
-        double precoFinal2 = p2.preco * (1 - p2.desconto);
+        double precoFinal1 = p1.precoComDesconto(0, 1.4);
+        double precoFinal2 = p2.precoComDesconto(0.1, 4.4);
 
         double mediaCarrinho = (precoFinal1 + precoFinal2) / 2;
         System.out.printf("Média do carrinho = R$%.2f", mediaCarrinho);
 
+        //métodos tem ()
     }
 }
