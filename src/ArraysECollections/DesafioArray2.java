3a94da02d2ba22f702087bfcda6385c77f1fd29f
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2022-10-11 18:32:37 -0300

    DesafioArray2

---
+++ src/ArraysECollections/DesafioArray2.java
@@ -0,0 +1,31 @@
+package ArraysECollections;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class DesafioArray2 {
+    public static void main(String[] args) {
+        Locale.setDefault(new Locale("en", "US"));
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Quantas notas: ");
+
+        int qtdecNotas = entrada.nextInt();
+
+        double[] notas = new double[qtdecNotas];
+
+        for (int i = 0; i < notas.length; i++) {
+            System.out.print("Informe a nota " + i + 1 + ": ");
+            notas[i] = entrada.nextDouble();
+        }
+
+        double total = 0;
+        for (double nota: notas) {
+            total += nota;
+        }
+
+        System.out.println("A média é " + (total / notas.length));
+
+        entrada.close();
+    }
+}
